e63ada2082402a97ec6876b9226e19d08c58561b
runalsh <21105673+runalsh@users.noreply.github.com>
2024-05-27 20:29:36 +0300

    3+

--- 3-counts/python-to-p9s.py
+++ 3-counts/python-to-p9s.py
@@ -1,24 +1,58 @@
 import http.server
 import random
+from random import randint
+import time
+from time import sleep
+import unittest
 from prometheus_client import start_http_server
 from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Summary
+from prometheus_client import Histogram
+from prometheus_client import REGISTRY
 
 REQUESTS = Counter('hello_worlds_total','Hello Worlds requested.')
 EXCEPTIONS = Counter('hello_world_exceptions_total','Exceptions serving Hello World.')
 SALES = Counter('hello_world_sales_euro_total','Euros made serving Hello World.')
+INPROGRESS = Gauge('hello_worlds_inprogress','Number of Hello Worlds in progress.')
+LAST = Gauge('hello_world_last_time_seconds','The last time a Hello World was served.')
+TIME = Gauge('time_seconds','The current time.')
+LATENCY = Summary('hello_world_latency_seconds','Time for a request Hello World.')
+LATENCY_H = Histogram('hello_world_latency_histogram_seconds','Time for a request Hello World(histogram)')
+LATENCY_H_BUCKET = Histogram('hello_world_latency_histogram_bucket_seconds','Time for a request Hello World(histogram+bucket)',buckets=[0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1])
+FOOS = Counter('foos_total', 'The number of foo calls.')
+
+def foo():
+    FOOS.inc()
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         REQUESTS.inc()
+        INPROGRESS.inc()
         with EXCEPTIONS.count_exceptions():
             if random.random() < 0.2:
                 raise Exception
         euros = random.random()
         SALES.inc(euros)
+        start = time.time()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
         self.wfile.write("Hello World for {} euros.".format(euros).encode())
+        LAST.set(time.time())
+        TIME.set_function(lambda: time.time())
+        INPROGRESS.dec()
+        sleep(randint(0.01,0.5))
+        LATENCY.observe(time.time() - start)
+        LATENCY_H.observe(time.time() - start)
+        LATENCY_H_BUCKET.observe(time.time() - start)
+
+class TestFoo(unittest.TestCase):
+    def test_counter_inc(self):
+        before = REGISTRY.get_sample_value('foos_total')
+        foo()
+        after = REGISTRY.get_sample_value('foos_total')
+        self.assertEqual(1, after - before)
 
 if __name__ == "__main__":
     start_http_server(8000)
